8bea8b0d70f22a446023ed65876807bf4b7059bc
Moideen Parampil Mohamed Reyas <mreyas@us.ibm.com>
2019-10-20 22:19:16 -0500

    fix image path for output image

--- build/build.sh
+++ build/build.sh
@@ -8,8 +8,8 @@ echo "Content of the file $(cat $PUSH_DOCKERCFG_PATH/.dockercfg)"
 (echo "{ \"auths\": " ; sudo cat $PUSH_DOCKERCFG_PATH/.dockercfg ; echo "}") > /tmp/.dockercfg
 echo "OUTPUT_REGISTRY" ${OUTPUT_REGISTRY}
 echo "RELEASE_VERSION" ${RELEASE_VERSION}
-buildah tag om-agent:10.0 ${OUTPUT_REGISTRY}/${RELEASE_VERSION}
+buildah tag om-agent:10.0 ${OUTPUT_REGISTRY}/${OMS_AGENT_IMG_STREAM}:${RELEASE_VERSION}
 echo "buildah tag done"
 sudo docker images
-buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${RELEASE_VERSION}
+buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OMS_AGENT_IMG_STREAM}:${RELEASE_VERSION}
 echo "buildah push done"
